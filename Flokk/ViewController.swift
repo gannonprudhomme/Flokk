1a8633f015ed8d0eaa714cc547bc00e5ace74736
Gannon Prudomme <gannonprudhomme@gmail.com>
2018-07-06 23:07:15 -0500

    Initial Commit

---
+++ Flokk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flokk
+//
+//  Created by Gannon Prudomme on 7/6/18.
+//  Copyright © 2018 Gannon Prudomme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
