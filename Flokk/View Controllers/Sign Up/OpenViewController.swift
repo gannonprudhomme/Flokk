4e476013bfc27c454f1e87ce5b2b3536f33d9fe0
heyenhigher <jaredheyen123@gmail.com>
2018-08-24 20:29:00 -0500

    Hid nav bar for opening screen

---
+++ Flokk/View Controllers/Sign Up/OpenViewController.swift
@@ -0,0 +1,49 @@
+//
+//  OpenViewController.swift
+//  Flokk
+//
+//  Created by Jared Heyen on 8/23/18.
+//  Copyright © 2018 Gannon Prudomme. All rights reserved.
+//
+
+import UIKit
+
+class OpenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the Navigation Bar
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the Navigation Bar
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
