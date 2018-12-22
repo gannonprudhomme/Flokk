3ed7eafa67df26919be4e36460356f73a33513bb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2018-12-21 23:16:32 -0600

    Further implemented GroupModelController

--- Flokk/Model Controllers/GroupModelController.swift
+++ Flokk/Model Controllers/GroupModelController.swift
@@ -7,22 +7,73 @@
 //
 
 import Foundation
+import UIKit
 import Promises
 
 // Contains a global instance of all of the groups within the app
 // Probably store them in a hashmap/hastable, with their uid as they key
 // Also provide functions to retrieve group data from the database
 class GroupModelController {
-    var groups: [String : GroupModel]! = nil // Ideally this is a hashmap
+    var groups = [String : GroupModel]() // Ideally this is a hashmap
     
+    // Add the group to the map
+    func addGroup(group: GroupModel) {
+        groups[group.uid] = group
+    }
     
-    // Load the group and
-    func loadGroup(uid: String) -> Promise<String> {
+    // Load the following group data: creationDate, creator, name, icon?
+    func loadGroup(uid: String, name: String) -> Promise<GroupModel> {
         // Immediately register this group in the map so we don't double load
-        return Promise<String> { fulfill, reject in
+        return Promise<GroupModel> { fulfill, reject in
+            // var model = GroupModel(uid: uid, name: name) // Create the GroupModel object
             
+            if groups.contains(where: { key, value in
+                return key == uid}) {
+                
+                reject("Error: Wrong")
+            } else {
+                // Add the group to the hashtable
+                groups[uid] = GroupModel(uid: uid, name: name)
+            }
             
+            // First check if we have this file stored locally
+            if LocalFileController.doesGroupFileExist(uid: uid) {
+                // If we do, load it in locally
+                
+            } else {
+                // Didn't load the group from a local file, load it in from the database and (maybe) save it to a file
+                let groupData: [String : Any]! = nil
+                
+                self.loadGroupFromDatabase(uid: uid)
+            }
+        }
+    }
+    
+    // Load a dict of the group's members containing their uid and name
+    func loadGroupMembers(uid: String) -> Promise<[String : String]> {
+        return Promise{ fulfill, reject in
+            // First check if the Group is loaded & exists in the database
+            let ret = [String : String]()
             
+            fulfill(ret)
+        }
+
+    }
+    
+    // Load the group's icon from Storage
+    func loadGroupIcon(uid: String) -> Promise<UIImage?> {
+        return Promise{ fulfill, reject in
+            // First check if the Group is loaded & exists in the database
+            fulfill(nil)
+        }
+    }
+}
+
+extension GroupModelController {
+    // Should this be in a separate file?
+    func loadGroupFromDatabase(uid: String) -> Promise<GroupModel> {
+        return Promise{ fulfill, reject in
+            fulfill(GroupModel(uid: uid))
         }
     }
 }
--- Flokk/Models/GroupModel.swift
+++ Flokk/Models/GroupModel.swift
@@ -11,8 +11,15 @@ import UIKit
 
 class GroupModel {
     var uid: String
-    var name: String
+    var name: String?
     var icon: UIImage? // Need to ensure this is only loaded in once
+    
+    var members: [String]? // Array of the members' uids
+    
+    // Might be used when we don't know anything about the group, before we load it in
+    init(uid: String) {
+        self.uid = uid
+    }
     
     init(uid: String, name: String) {
         self.uid = uid
@@ -21,7 +28,5 @@ class GroupModel {
         icon = nil
     }
     
-    // Set icon once it's loaded in?
-    // Call it from whatever handles the icons - GroupModelController?f
-    
+    func addMember(uid: String) { }
 }
