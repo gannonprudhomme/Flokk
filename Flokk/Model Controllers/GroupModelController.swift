f0b0e8b63ccea41c3e33eb9c0801ca4f1b868f0b
Gannon Prudhomme <gannonprudhomme@gmail.com>
2018-12-21 20:13:23 -0600

    Added Group and User model controllers

---
+++ Flokk/Model Controllers/GroupModelController.swift
@@ -0,0 +1,23 @@
+//
+//  GroupModelController.swift
+//  Flokk
+//
+//  Created by Gannon Prudhomme on 12/21/18.
+//  Copyright © 2018 Gannon Prudomme. All rights reserved.
+//
+
+import Foundation
+//import UIKit
+
+// Contains a global instance of all of the groups within the app
+// Probably store them in a hashmap/hastable, with their uid as they key
+// Also provide functions to retrieve group data from the database
+class GroupModelController {
+    var groups: [String : GroupModel]! = nil
+    
+    
+    
+    func loadGroup() {
+        
+    }
+}
---
+++ Flokk/Model Controllers/UserModelController.swift
@@ -0,0 +1,36 @@
+//
+//  UserModelController.swift
+//  Flokk
+//
+//  Created by Gannon Prudhomme on 12/21/18.
+//  Copyright © 2018 Gannon Prudomme. All rights reserved.
+//
+
+import Foundation
+//import UIKit
+
+// Contains a global instance of all of the users within the app
+// Also provides functions to load in users given their uid (and other parameters)
+// Its purpose is basically that it prevents view controllers from a having to have to deal with retrieveing user data by themselves
+class UserModelController {
+    var uid: String
+    var handle: String?
+    var fullName: String?
+    // var email: String?
+    
+    var profilePhoto: UIImage?
+    
+    var groups: [String]? // List of the uid's for the groups this user is in
+    
+    init(uid: String) {
+        self.uid = uid
+        self.handle = nil
+        self.fullName = nil
+        
+        self.profilePhoto = nil
+    }
+    
+    
+    
+    // function convertToDict() -> [String : Any] { } // Is this necessary/in the right place?
+}
