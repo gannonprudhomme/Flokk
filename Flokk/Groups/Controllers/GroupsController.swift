b889efe063020439217d15f53d355e67a3f7c702
Gannon Prudhomme <gannonprudhomme@gmail.com>
2018-12-29 02:54:41 -0600

    Outlined GroupsController functions and added GroupsViewController basic initialization

--- Flokk/Groups/Controllers/GroupsController.swift
+++ Flokk/Groups/Controllers/GroupsController.swift
@@ -11,5 +11,25 @@ import Foundation
 // Handles any control involved with the Groups View (Controller)
 // Handles group loading, authentication(?), 
 class GroupsController {
+    // Determine whether or not a user is signed in, and if their Flokk data is loaded
+    // in yet.
+    func handleAuthentication() {
+        // If authenticated, but no Flokk user data
+            // load user & group data
+        
+        // If not authenticated
+            // Send the user to Sign Up / Log In
+    }
+    
+    func initialLoad() {
+        // Load relevant group data
+        
+    }
+    
+    // Ideally, not much logic in here
+    func loadUserData() {
+        // Call some other handler function to load the necessary user ddata for the main user
+    }
+    
     
 }
--- Flokk/Groups/Views/GroupsViewController.swift
+++ Flokk/Groups/Views/GroupsViewController.swift
@@ -15,24 +15,29 @@ import UIKit
 // Should I consider renaming this to GroupsView or GroupsViewC
 class GroupsViewController: UIViewController {
     // Instance of GroupsController?
+    private var groupsController = GroupsController()
+    private var tableViewController =  GroupsTableViewController()
     
     @IBOutlet weak var tableView: UITableview
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Initialize Groups controller
 
         // Set tableView delegate & data source
+        tableView.delegate = tableViewController
+        tableView.dataSource = tableViewController
         
         // Handle User Authentication
-        
+        groupsController.handleAuthentication()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
     }
 }
 
-// Table View functions for use from GroupsController
+// Table View interfacing functions for use from GroupsController
 extension GroupsViewController {
     
 }
